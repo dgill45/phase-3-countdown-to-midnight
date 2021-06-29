28fc59e0fe543b5029afa3fe14ebe06c66559ac6
donellgill <donell.gill@outlook.com>
2021-06-29 04:57:44 -0400

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
 
-def countdown
+number = 20
+
+def countdown(number)
+    
+    while number != 0
+        puts "#{number} SECOND(S)!"
+        number -= 1    
+        break if number == 0
+    end 
+     return "HAPPY NEW YEAR!"
+end
+
+  number = 20
+
+def countdown_with_sleep(number)
+    
+    while number != 0 
+        puts "#{number} SECOND(S)!"
+        sleep(1)
+        number -= 1
+        break if number == 0
+    end
+    return "HAPPY NEW YEAR!"
 end
